81e96ccb6bb66f88ba2ca6717c41f3f4398fa9e7
Mykola <chmutkola@gmail.com>
2018-04-22 23:05:51 +0300

    add joi

--- routes/addUser.js
+++ routes/addUser.js
@@ -5,12 +5,29 @@ let url = 'mongodb://Ivan:Ivan@cluster0-shard-00-00-n5kxm.mongodb.net:27017,clus
 let MongoClient = require('mongodb').MongoClient;
 let assert = require('assert');
 
+const Joi = require('joi');
+ 
+const schema = Joi.object().keys({
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+    // access_token: [Joi.string(), Joi.number()],
+    // birthyear: Joi.number().integer().min(1900).max(2013),
+    email: Joi.string().email()
+}).with('username', 'birthyear').without('password', 'access_token');
+
 router.post('*', function (req, res, next) {
-    console.log('hello')
+
     MongoClient.connect(url, function (err, db) {
+
+        Joi.validate(req.body, schema, function (err, value) { 
+            if ( err ) {
+                res.json(err.message)
+            }
+        }); 
 
         assert.equal(null, err);
         let collection = db.collection('name');
+ 
 
         collection.find({ "email": req.body.email })
             .toArray(function (err, docs) {
@@ -31,6 +48,7 @@ router.post('*', function (req, res, next) {
 
         collection.insert({
             "name": req.body.name,
+            "password": req.body.password,
             "email": req.body.email
         }, function (err, docs) {
             if (err) {
