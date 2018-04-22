eeff06ccbc7e3c10001060beb5ce6b62db66f0df
Mykola <chmutkola@gmail.com>
2018-04-22 22:05:07 +0300

    add working mongoDB

--- app.js
+++ app.js
@@ -1,72 +1,76 @@
 
 var app = require('./settings');
-var url = 'mongodb://Chmut:1111>@name-shard-00-00-pw0kg.mongodb.net:27017,name-shard-00-01-pw0kg.mongodb.net:27017,name-shard-00-02-pw0kg.mongodb.net:27017/name?ssl=true&replicaSet=name-shard-0&authSource=admin' 
+var url = 'mongodb://Ivan:Ivan@cluster0-shard-00-00-n5kxm.mongodb.net:27017,cluster0-shard-00-01-n5kxm.mongodb.net:27017,cluster0-shard-00-02-n5kxm.mongodb.net:27017/MyMonDb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
+// var url = 'mongodb://localhost:27017/name'; 
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var error = require('./routes/error');
 
 app.post('/products', function (req, res, next) {
-  console.log('file')
-  MongoClient.connect(url, function(err, db) {
-    
+
+  MongoClient.connect(url, function (err, db) {
     assert.equal(null, err);
     var collection = db.collection('name');
-   
-   collection.insert({
-     name: req.body.name,
-     gender: req.body.email
-   }, function(err, docs) {
-     console.log(docs)
-    if (err) {
-      console.log(err)
-      
-      res.json(err)
-      db.close();
-    } else {
-      console.log(docs)
-      res.json(docs);
-      db.close();
-    }
-  // collection.find({}).toArray ( function (err, docs) {
-  //   console.log(docs);
-  //   res.json(docs);
-    // db.close();
+
+    collection.insert({
+      name: req.body.name,
+      gender: req.body.email
+    }, function (err, docs) {
+      if (err) {
+        res.json(err)
+        db.close();
+      } else {
+        res.json(docs);
+        db.close();
+      }
+    })
   })
-   })
-  // res.send(req.body)
 });
 
+app.post('/find', function (req, res, next) {
+  MongoClient.connect(url, function (err, db) {
+
+    assert.equal(null, err);
+    var collection = db.collection('name');
+
+    collection.find({}).toArray(function (err, docs) {
+      res.json(docs);
+      db.close();
+    })
+  })
+})
 app.get('/hello', function (req, res, next) {
-  res.json({'hello': 'man'})
+  res.json({ 'hello': 'man' })
 })
 
 
 app.get('/man', function (req, res, next) {
- const options = {
-  method: 'POST',
-  uri: 'https://jsonplaceholder.typicode.com/posts',
-  body : {
-    hello: "man"
-  }
- };
+  const options = {
+    method: 'POST',
+    uri: 'https://jsonplaceholder.typicode.com/posts',
+    body: {
+      hello: "man"
+    }
+  };
   request(options)
- .then(function (response) {
-     // Запрос был успешным, используйте объект ответа как хотите
-     res.json(response)
- })
- .catch(function (err) {
-  res.json({response: 'error'})
- })
-  
+    .then(function (response) {
+      res.json(response)
+    })
+    .catch(function (err) {
+      res.json({ response: 'error' })
+    })
+
 })
 
 app.get('/products', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for a whitelisted domain.'})
+  res.json({ msg: 'This is CORS-enabled for a whitelisted domain.' })
 })
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
- app.use('*', error);
+app.use('*', error);
 
 module.exports = app;
