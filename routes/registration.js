ae72ee55a29848a48ac79f41f34d49e12d51aa02
Mykola <chmutkola@gmail.com>
2018-05-13 19:56:39 +0300

    add name

--- routes/registration.js
+++ routes/registration.js
@@ -7,7 +7,8 @@ let connect1 = require('../db');
 const Joi = require('joi');
 
 const schema = Joi.object().keys({
-    name: Joi.string().alphanum().min(3).max(30).required(),
+    firstName: Joi.string().alphanum().min(3).max(30).required(),
+    lastName: Joi.string().alphanum().min(3).max(30).required(),
     password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
     email: Joi.string().email(),
     // access_token: [Joi.string(), Joi.number()],
